58e5513ecdb6b6c87d97b1f7e9d149c22cd4c599
Mulyana <mulcoding@gmail.com>
2024-01-29 15:56:46 +0700

    add new solution

--- 4_reverse_string.py
+++ 4_reverse_string.py
@@ -1,6 +1,9 @@
 # day 2/366
 
 def solution(string):
-    return string[::-1]
+    # return string[::-1]
+    temp = list(string)
+    temp.reverse()
+    return ''.join(temp)
 
 print(solution("testos"))

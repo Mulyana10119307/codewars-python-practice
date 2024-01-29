1ffa5c9c355af8440a0078a68f9d74a6b4d2960b
Mulyana <mulcoding@gmail.com>
2024-01-29 15:12:23 +0700

    day 2/366

---
+++ 3_return_negative.py
@@ -0,0 +1,7 @@
+def make_negative( number ):
+#     return number if number < 0 else number * -1
+    return -(abs(number))
+
+print(make_negative(34))
+print(make_negative(-231))
+print(make_negative(123))
